ec9806c21185a88f9639aa20556eeaee12fef776
bojana <b.janjicic@gmail.com>
2020-02-05 11:10:37 +0100

    initial setup

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+
+class Index extends React.Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <h1>
+          Welcome to <br /> Will&Skill
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Index;
