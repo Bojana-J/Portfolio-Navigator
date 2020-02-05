4b429e30431d2a4915f50188491c05b5949b7d4f
bojana <b.janjicic@gmail.com>
2020-02-05 12:44:12 +0100

    create login form

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 
 class Index extends React.Component {
   state = {};
@@ -5,9 +6,33 @@ class Index extends React.Component {
   render() {
     return (
       <div>
+        <Head>
+          <title>WillandSkill</title>
+          <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/minty/bootstrap.min.css"
+          />
+        </Head>
+
         <h1>
           Welcome to <br /> Will&Skill
         </h1>
+
+        <div className="container">
+          <form>
+            <div className="form-group">
+              <label htmlFor="email">Email address</label>
+              <input type="email" className="form-control" id="email" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" className="form-control" id="password" />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Sign in
+            </button>
+          </form>
+        </div>
       </div>
     );
   }
